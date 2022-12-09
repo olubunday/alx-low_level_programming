1cffbc997c1307fedddf4c809744a6968364ce43
olubunday <adeolaolumide9@gmail.com>
2022-12-09 14:38:32 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,9 @@ int main(void)
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 	for (la = 'a'; la <= 'f'; la++)
-		putchar('\n');
+		putchar(la);
+
+	putchar('\n');
 
 	return (0);
 }
