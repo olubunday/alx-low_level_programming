f2ef402418fc37001df4f995b4d2285362cd2826
olubunday <adeolaolumide9@gmail.com>
2022-12-13 08:31:21 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	-putchar('\n');
+}
