154a104eefe362abf486d57f0b7bb0df0215245f
olubunday <adeolaolumide9@gmail.com>
2022-12-13 13:14:58 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,13 @@
  */
 int print_last_digit(int n)
 {
-	int x;
+	int last;
 
-	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar(x + '0');
-	return (x);
+	last = n % 10;
+
+	if (last < 0)
+		last = last * -1;
+	_putchar(last + '0');
+
+	return (last);
 }
