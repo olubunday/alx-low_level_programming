a1f0b31aff8c8e1ff4d821f6264a32ff6f02ecc5
olubunday <adeolaolumide9@gmail.com>
2022-12-15 14:48:31 -0800

    Tadsk 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,14 +11,14 @@ int main(void)
 	long int n, fp;
 
 	n = 612852475143;
-	for (fp = 2; fp <= n; fp++)
+	for (fp = 2; fp < n; fp++)
 	{
 		if (n % fp == 0)
 		{
-			n /= fp;
+			n = n / fp;
 			fp--;
 		}
 	}
-	printf("%ld\n", fp);
+	printf("%lu\n", fp);
 	return (0);
 }
