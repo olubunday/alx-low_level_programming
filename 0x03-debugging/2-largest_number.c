e0985ea3fc23f7a71b89df337b2bc2ee07a26d2c
olubunday <adeolaolumide9@gmail.com>
2022-12-15 16:27:15 -0800

    Task 2 debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,16 +20,14 @@ int largest_number(int a, int b, int c)
 			largest = a;
 		}
 	}
-	else
-	{
-		if (c > a)
-		{
-			largest = c;
-		}
-	}
+
 	else if (b > c)
 	{
 		largest = b;
+	}
+	else
+	{
+		largest = c;
 	}
 
 	return (largest);
