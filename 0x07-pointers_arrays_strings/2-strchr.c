6aa133697720300e9a87b82005162717ae0a2693
olubunday <adeolaolumide9@gmail.com>
2023-01-02 14:36:22 -0800

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: string
+ * @c: character to search for
+ * Return: Pointer to the first occurence of the character c in the string
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		++s;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+
+	return (0);
+}
+
