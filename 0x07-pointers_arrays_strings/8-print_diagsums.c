5adc7841295901674e5a80e33d22ff4752146d94
olubunday <adeolaolumide9@gmail.com>
2023-01-02 19:06:47 -0800

    Task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
-#include "main.h":q
+#include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - prints sum of a square
  * @a: pointer to array
@@ -9,4 +10,22 @@
 void print_diagsums(int *a, int size)
 {
 	int i, size1;
-	unsigned int sumbi
+	unsigned int sumDiag1, sumDiag2;
+
+	size1 = 0;
+	sumDiag1 = 0;
+	sumDiag2 = 0;
+
+	size1 = (size * size) - 1;
+
+	for (i = 0; i <= size1; i = i + (size + 1))
+	{
+		sumDiag1 = sumDiag1 + a[i];
+	}
+	for (i = (size - 1); i < size1; i = i + (size - 1))
+	{
+		sumDiag2 = sumDiag2 + a[i];
+	}
+	printf("%d, %d\n", sumDiag1, sumDiag2);
+}
+
