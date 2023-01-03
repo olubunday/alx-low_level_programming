23bd22f55fa6981bffbeded3769ec500eafac477
olubunday <adeolaolumide9@gmail.com>
2023-01-02 18:53:42 -0800

    Task 6

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "main.h":q
+/**
+ * print_diagsums - prints sum of a square
+ * @a: pointer to array
+ * @size: size of array
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, size1;
+	unsigned int sumbi
