63cd323362b872af843b4dcae75d4ad79220d651
olubunday <adeolaolumide9@gmail.com>
2023-01-06 08:26:02 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 
 /**
  * main - Write a program that prints its name, followed by a new line
- * If you rename the program, it will print the new line, without having to
+ * If you rename the program, it will print the new name, without having to
  * compile it again
- * You should not remove the path beforre the name of the program
- * 
+ * You should not remove the path before the name of the program
+ *
  * @argc: This is the argument count
  * @argv: This is the argument vector
  *
