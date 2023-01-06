bca5c0ddd3254928aeb59502d3a0e74d91e3ed19
olubunday <adeolaolumide9@gmail.com>
2023-01-06 05:52:23 -0800

    Task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns thr the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
