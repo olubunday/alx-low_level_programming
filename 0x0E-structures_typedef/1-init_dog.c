1284afcb537539f9ff458284d012c3671126b1f3
olubunday <adeolaolumide9@gmail.com>
2023-01-17 06:41:51 -0800

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
 *
 * Return: void
 */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == 0)
 		return;
