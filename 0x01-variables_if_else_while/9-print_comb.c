bed406d855325c9ca57285b7a4cc0a1dbe0d5573
olubunday <adeolaolumide9@gmail.com>
2023-03-04 07:40:31 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,13 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
-
-	putchar(n);
-	if (n != 57)
+	for (n = 0; n < 10; n++)
 	{
+		putchar(n + '0');
 		putchar(',');
 		putchar(' ');
 	}
+
 	putchar('\n');
 	return (0);
 }
